008285200eac6f881750882cf7b11c53063f555b
zed1606 <32841752+zed1606@users.noreply.github.com>
2022-06-09 07:10:05 +0700

    Initial commit

---
+++ src/pages/Overview.jsx
@@ -0,0 +1,236 @@
+import React from 'react';
+import { GoPrimitiveDot } from 'react-icons/go';
+import { IoIosMore } from 'react-icons/io';
+import { DropDownListComponent } from '@syncfusion/ej2-react-dropdowns';
+import { IoRocketOutline } from 'react-icons/io5';
+import { GiMineExplosion } from 'react-icons/gi';
+
+import { Stacked, Pie, Button, SparkLine } from '../components';
+import { earningData, recentTransactions, weeklyStats, dropdownData, SparklineAreaData, ecomPieChartData } from '../data/dummy';
+import { useStateContext } from '../contexts/ContextProvider';
+import product9 from '../data/product9.png';
+
+const DropDown = ({ currentMode }) => (
+  <div className="w-28 border-1 border-color px-2 py-1 rounded-md">
+    <DropDownListComponent id="time" fields={{ text: 'Time', value: 'Id' }} style={{ border: 'none', color: (currentMode === 'Dark') && 'white' }} value="1" dataSource={dropdownData} popupHeight="220px" popupWidth="120px" />
+  </div>
+);
+
+const Overview = () => {
+  const { currentColor, currentMode } = useStateContext();
+
+  return (
+    <div className="mt-24">
+      <div className="flex flex-wrap lg:flex-nowrap justify-center">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-96 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
+          <div className="flex justify-between items-center">
+            <div>
+              <div>
+                <p className="font-bold text-yellow-300">Hotest Project</p>
+                <p className="text-2xl text-white">Homieverse</p>
+              </div>
+            </div>
+          </div>
+          <div className="mt-6">
+            <Button
+              color="white"
+              bgColor={currentColor}
+              text="Go to Project"
+              borderRadius="10px"
+            />
+          </div>
+        </div>
+        <div className="flex m-3 flex-wrap justify-center gap-5 items-center">
+          {earningData.map((item) => (
+            <div key={item.title} className="bg-white h-44 dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56  p-4 pt-9 rounded-2xl ">
+              <button
+                type="button"
+                style={{ color: item.iconColor, backgroundColor: item.iconBg }}
+                className="text-2xl opacity-0.9 rounded-full  p-4 hover:drop-shadow-xl"
+              >
+                {item.icon}
+              </button>
+              <p className="mt-3">
+                <span className="text-lg font-semibold">{item.amount}</span>
+                <span className={`text-sm text-${item.pcColor} ml-2`}>
+                  {item.percentage}
+                </span>
+              </p>
+              <p className="text-sm text-gray-400  mt-1">{item.title}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="flex gap-10 flex-wrap justify-center">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780  ">
+          <div className="flex justify-between">
+            <p className="font-semibold text-xl">Community Pulse</p>
+            <div className="flex items-center gap-4">
+              <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+                <span>Crash Landing</span>
+              </p>
+              <p className="flex items-center gap-2 text-green-400 hover:drop-shadow-xl">
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+                <span>To the Moon</span>
+              </p>
+            </div>
+          </div>
+          <div className="mt-10 flex gap-10 flex-wrap justify-center">
+            <div className=" border-r-1 border-color m-4 pr-10">
+              <div>
+                <p>
+                  <span className="flex gap-3 text-3xl font-semibold"><IoRocketOutline /> 9652</span>
+                </p>
+                <p className="text-gray-500 mt-1">To the Moon</p>
+              </div>
+              <div className="mt-8">
+                <p className="flex gap-3 text-3xl font-semibold"><GiMineExplosion /> 523</p>
+
+                <p className="text-gray-500 mt-1">Crash Landing</p>
+              </div>
+
+              <div className="mt-5">
+                <SparkLine currentColor={currentColor} id="line-sparkLine" type="Line" height="80px" width="250px" data={SparklineAreaData} color={currentColor} />
+              </div>
+            </div>
+            <div>
+              <Stacked currentMode={currentMode} width="320px" height="360px" />
+            </div>
+          </div>
+        </div>
+        <div>
+          <div
+            className=" rounded-2xl md:w-400 p-4 m-3"
+            style={{ backgroundColor: currentColor }}
+          >
+            <div className="flex justify-between items-center ">
+              <p className="font-semibold text-white text-2xl">Trades</p>
+
+              <div>
+                <p className="text-2xl text-white font-semibold mt-8">1578N</p>
+                <p className="text-gray-200">Monthly trades</p>
+              </div>
+            </div>
+
+            <div className="mt-4">
+              <SparkLine currentColor={currentColor} id="column-sparkLine" height="100px" type="Column" data={SparklineAreaData} width="320" color="rgb(242, 252, 253)" />
+            </div>
+          </div>
+
+          <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl md:w-400 p-8 m-3 flex justify-center items-center gap-10">
+            <div>
+              <p className="text-2xl font-semibold ">5896N</p>
+              <p className="text-gray-400">Yearly trades</p>
+            </div>
+
+            <div className="w-40">
+              <Pie id="pie-chart" data={ecomPieChartData} legendVisiblity={false} height="160px" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-wrap justify-center">
+        <div className="flex gap-10 m-4 flex-wrap justify-center">
+          <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg p-6 rounded-2xl">
+            <div className="flex justify-between items-center gap-2">
+              <p className="text-xl font-semibold">Top Projects</p>
+              <DropDown currentMode={currentMode} />
+            </div>
+            <div className="mt-10 w-72 md:w-400">
+              {recentTransactions.map((item) => (
+                <div key={item.title} className="flex justify-between mt-4">
+                  <div className="flex gap-4">
+                    <div>
+                      <p className="text-md font-semibold">{item.title}</p>
+                      <p className="text-sm text-gray-400">{item.desc}</p>
+                    </div>
+                  </div>
+                  <p className={`text-${item.pcColor}`}>{item.amount}</p>
+                </div>
+              ))}
+            </div>
+
+          </div>
+
+        </div>
+
+        <div className="md:w-400 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6 m-3">
+          <div className="flex justify-between">
+            <p className="text-xl font-semibold">Weekly Stats</p>
+            <button type="button" className="text-xl font-semibold text-gray-500">
+              <IoIosMore />
+            </button>
+          </div>
+
+          <div className="mt-10 ">
+            {weeklyStats.map((item) => (
+              <div key={item.title} className="flex justify-between mt-4 w-full">
+                <div className="flex gap-4">
+                  <button
+                    type="button"
+                    style={{ background: item.iconBg }}
+                    className="text-2xl hover:drop-shadow-xl text-white rounded-full p-3"
+                  >
+                    {item.icon}
+                  </button>
+                  <div>
+                    <p className="text-md font-semibold">{item.title}</p>
+                    <p className="text-sm text-gray-400">{item.desc}</p>
+                  </div>
+                </div>
+
+                <p className={`text-${item.pcColor}`}>{item.amount}</p>
+              </div>
+            ))}
+            <div className="mt-4">
+              <SparkLine currentColor={currentColor} id="area-sparkLine" height="160px" type="Area" data={SparklineAreaData} width="320" color="rgb(242, 252, 253)" />
+            </div>
+          </div>
+
+        </div>
+
+        <div className="w-400 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6 m-3">
+          <div className="flex justify-between">
+            <p className="text-xl font-semibold">Hot Pick</p>
+            <button type="button" className="text-xl font-semibold text-gray-500">
+              <IoIosMore />
+            </button>
+          </div>
+          <div className="mt-10">
+            <img
+              className="md:w-96 h-50 "
+              src={product9}
+              alt=""
+            />
+            <div className="mt-8">
+              <p className="font-semibold text-lg">Homieverse Tech</p>
+              <p className="text-gray-400 ">Technology & Art Comics</p>
+              <p className="mt-8 text-sm text-gray-400">
+                This will be the small description for the news you have shown
+                here. There could be some great info.
+              </p>
+              <div className="mt-3">
+                <Button
+                  color="white"
+                  bgColor={currentColor}
+                  text="Find Out More"
+                  borderRadius="10px"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Overview;
---
+++ src/pages/index.jsx
@@ -0,0 +1,7 @@
+export { default as NFTs } from './NFTs';
+export { default as Projects } from './Projects';
+export { default as Calendar } from './Calendar';
+export { default as Line } from './Charts/Line';
+export { default as Pie } from './Charts/Pie';
+export { default as Stacked } from './Charts/Stacked';
+export { default as Overview } from './Overview';
